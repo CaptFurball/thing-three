4524d9f65f6e73bf04041a4689439d9cf37f12b2
test <test@mail.com>
2021-02-18 21:30:03 +0800

    Updated character sprite

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
-var velocity
+export var speed : int = 2
+
+var velocity : Vector2
 
 func _ready():
 	pass # Replace with function body.
@@ -9,15 +11,27 @@ func _physics_process(_delta):
 	velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 5
+		velocity.y -= speed
+		$AnimatedSprite.play("bounce_back")
 	
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 5
+		velocity.y += speed
+		$AnimatedSprite.play("bounce_front")
 		
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 5
+		velocity.x -= speed
+		$AnimatedSprite.play("bounce_side")
+		$AnimatedSprite.flip_h = true		
 		
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 5
+		velocity.x += speed
+		$AnimatedSprite.play("bounce_side")
+		$AnimatedSprite.flip_h = false
 
 	move_and_collide(velocity)
+
+
+func _on_AnimatedSprite_animation_finished():
+#	$AnimatedSprite.play("idle_1")
+	$AnimatedSprite.stop()
+	pass # Replace with function body.
