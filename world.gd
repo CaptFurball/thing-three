2bad190e0fa22083155012d56b7a9adf0445596d
CaptFurball <elys.1993a@gmail.com>
2021-02-23 00:54:13 +0800

    Code refactoring

--- world.gd
+++ world.gd
@@ -9,59 +9,51 @@ export(int, 5, 320, 1) var map_size_y : int = 10
 export var lane_width : int = 1
 export var wall_width : int = 1
 
+onready var floor_map : TileMap = $Floor
+onready var wall_map  : TileMap = $Wall
+
 func _ready():
 	var map_size : Vector2 = Vector2(map_size_x, map_size_y)
 	var maze  = Maze.new(map_size, lane_width, wall_width)
 	var tiles = maze.create_maze()
 
-	draw_floor(tiles)
-	draw_walls(tiles)
-	
+	self._draw_floor(tiles)
+	self._draw_walls(tiles)
+	self._set_player(maze.start_tile)
+
+func _set_player(start_tile : Vector2):
 	var player = Player.instance()
-	player.position = maze.start_tile * 16 + Vector2(8, 8)
+	player.position = start_tile * 16 + Vector2(8, 8)
 	add_child(player)
 
-func draw_floor(tiles):
-	var x = tiles.size()
-	var y = tiles[0].size()
-
-	var tile_map : TileMap = get_node("Floor")
-
-	for col in x:
-		for row in y:
+func _draw_floor(tiles):
+	for col in tiles.size():
+		for row in tiles.front().size():
 			var tile_name : String = tiles[col][row].type
-			var tile_set_idx : int = tile_map.tile_set.find_tile_by_name(tile_name)
+			var tile_set_idx : int = self.floor_map.tile_set.find_tile_by_name(tile_name)
 			
 			if tile_name == Tile.TYPE_PATH:
-				var rng = RandomNumberGenerator.new()
-				rng.randomize()
-				
 				var sub_tile = self._get_subtile_coord(tile_set_idx)
-				tile_map.set_cell(col, row, tile_set_idx, false, false, false, sub_tile)
+				self.floor_map.set_cell(col, row, tile_set_idx, false, false, false, sub_tile)
 			else:
-				tile_map.set_cell(col, row, tile_set_idx)
-			
-	tile_map.update_bitmask_region(Vector2.ZERO, Vector2(x - 1, y - 1))
+				self.floor_map.set_cell(col, row, tile_set_idx)
 
-func draw_walls(tiles):
-	var x = tiles.size()
-	var y = tiles[0].size()
-
-	var tile_map : TileMap = get_node("Wall")
-
-	for col in x:
-		for row in y:
+func _draw_walls(tiles):
+	for col in tiles.size():
+		for row in tiles.front().size():
 			var tile_name : String = tiles[col][row].type
-			var tile_set_idx : int = tile_map.tile_set.find_tile_by_name(tile_name)
+			var tile_set_idx : int = self.wall_map.tile_set.find_tile_by_name(tile_name)
 			
 			if tile_name == Tile.TYPE_WALL:
-				tile_map.set_cell(col, row, tile_set_idx)
+				self.wall_map.set_cell(col, row, tile_set_idx)
 			
-	tile_map.update_bitmask_region(Vector2.ZERO, Vector2(x - 1, y - 1))
+	self.wall_map.update_bitmask_region(Vector2.ZERO, Vector2(tiles.size() - 1, tiles.front().size() - 1))
 
-func _get_subtile_coord(id):
-	var tiles = $Floor.tile_set
-	var rect = $Floor.tile_set.tile_get_region(id)
+func _get_subtile_coord(id : int) -> Vector2:
+	var tiles = self.floor_map.tile_set
+	var rect  = tiles.tile_get_region(id)
+
 	var x = randi() % int(rect.size.x / tiles.autotile_get_size(id).x)
 	var y = randi() % int(rect.size.y / tiles.autotile_get_size(id).y)
+	
 	return Vector2(x, y)
