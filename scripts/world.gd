44c0172bf1cc8c4ada7b82ec0ae3f650bc8ed8f7
CaptFurball <elys.1993a@gmail.com>
2021-02-24 15:53:08 +0800

    Updated

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,28 +1,35 @@
 extends Node2D
 
-var Maze   = preload("res://scripts/maze/maze.gd")
-var Tile   = preload("res://scripts/maze/tile.gd")
+var Maze = preload("res://scripts/maze/maze.gd")
+var Tile = preload("res://scripts/maze/tile.gd")
 var Player = preload("res://player.tscn")
 var TargetArea = preload("res://target_area.tscn")
 
-export(int, 5, 320, 1) var map_size_x : int = 10
-export(int, 5, 320, 1) var map_size_y : int = 10
+export var fog_mode: bool = true
+export(int, 5, 300, 1) var map_size_x : int = 10
+export(int, 5, 300, 1) var map_size_y : int = 10
 export var lane_width : int = 1
 export var wall_width : int = 1
 
 onready var floor_map : TileMap = $Floor
-onready var wall_map  : TileMap = $Wall
+onready var wall_map : TileMap = $Wall
+onready var fog : CanvasModulate = $Fog
 
 func _ready():
+	if fog_mode:
+		fog.color = Color(0, 0, 0)
+	else: 
+		fog.color = Color(1, 1, 1)
+		
 	var map_size : Vector2 = Vector2(map_size_x, map_size_y)
-	var maze  = Maze.new(map_size, lane_width, wall_width)
-	var tiles = maze.create_maze()
+	var maze = Maze.new(map_size, lane_width, wall_width)
+	maze.create_maze()
 
-	self._draw_floor(tiles)
-	self._draw_walls(tiles)
+	self._draw_floor(maze.get_tiles())
+	self._draw_walls(maze.get_tiles())
 	self._set_player(maze.start_tile)
 	self._set_win_area(maze.end_tile * 16 + Vector2(8, 8))
-
+	
 func _set_player(start_tile : Vector2):
 	var player = Player.instance()
 	player.position = start_tile * 16 + Vector2(8, 8)
@@ -37,7 +44,7 @@ func _draw_floor(tiles):
 			if tile_name == Tile.TYPE_PATH:
 				var sub_tile = self._get_subtile_coord(tile_set_idx)
 				self.floor_map.set_cell(col, row, tile_set_idx, false, false, false, sub_tile)
-			else:
+			elif tile_name != Tile.TYPE_WALL:
 				self.floor_map.set_cell(col, row, tile_set_idx)
 
 func _draw_walls(tiles):
